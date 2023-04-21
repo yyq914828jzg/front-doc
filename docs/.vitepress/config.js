71977799f581bf58309f477474cd00e716af1894
jiazhigang <jiazhigang@5irich.com>
2023-04-21 15:39:52 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,35 @@
+export default {
+  title: '创金工具库',
+  description: '组件、公共方法等',
+  themeConfig: {
+    nav: [
+      { text: "指南", link: "/guild/installation" },
+      { text: "组件", link: "/components/button" },
+    ],
+    socialLinks: [{ icon: "github", link: "http://git.5irich.com/richadmin/new-tc-admin.git" }],
+
+    sidebar: {
+      '/guild/': [
+        {
+          text:'基础',
+          collapsible: true,
+          items: [
+            {text:'安装',link:"/guild/installation"},
+            {text:'快速开始',link:"/guild/quickstart"},
+          ]
+        }
+      ],
+      '/components/': [
+        {
+          text: '组件',
+          collapsible: true,
+          items: [
+            { text: 'button', link: '/components/button' },
+            { text: 'table', link: '/components/table' },
+
+          ]
+        }
+      ]
+    }
+  }
+}
---
+++ docs/guild/quickstart.md
@@ -0,0 +1,19 @@
+# 快速开始
+
+本节将介绍如何在项目中使用 5irch-test
+
+## 用法
+
+...
+
+<div>
+<rich-button></rich-button>
+
+</div> 
+ 
+
+
+<script setup>
+    // import { richButton } from '5irich-test'
+</script>
+...
